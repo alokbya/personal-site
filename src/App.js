2a46d8dd8f22e9a0733d4cc9965982f697e87f17
alaaddin hiefield <alaaddin.hiefield@gmail.com>
2022-09-06 00:29:15 -0600

    removed Link - unused

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Navigation from './components/navigation.mjs';
 
 const App = () => {
