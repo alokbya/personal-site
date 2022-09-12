477f36dc86de06815097b0edb647f2cbcd55f573
Alaaddin Hiefield <alaaddin.hiefield@gmail.com>
2022-09-12 12:04:11 -0600

    Update intro

--- src/text/home-intro.mjs
+++ src/text/home-intro.mjs
@@ -1,4 +1,4 @@
-let intro = "I'm Alaaddin, but my friends call me Alaa-- and this is my homepage. There's not much here, but the idea is that this site will begin to flourish over time. I'm a software engineer with an academic background in mechanical engineering. I transitioned into software development a few years ago, and have not looked back. I'm very interested in learning new stuff. I don't always stick to a theme, and I find myself spending hours reading up on topics that don't cross over into my everyday life. I love starting personal projects, and I have yet to feel the joy of completing one. When can a personal project reach a final state anyway? Aren't all things in a constant state of change? Hopefully this homepage continues to grow with me, and keeps in a presentable state. Thanks for stopping by.";
+let intro = "I'm Alaaddin, but my friends call me Alaa.";
 
 export function getIntro()
 {
